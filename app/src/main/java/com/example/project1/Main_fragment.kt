26031c041be71874d03f51ee8463df85ab1ba488
ZZhanserik <200107046@stu.sdu.edu.kz>
2022-11-02 02:38:15 +0600

    commit nav2

--- app/src/main/java/com/example/project1/Main_fragment.kt
+++ app/src/main/java/com/example/project1/Main_fragment.kt
@@ -7,20 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 
-class Catalog : Fragment() {
+class Main_fragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.catalog, container, false)
+        return inflater.inflate(R.layout.fragment_main_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val bF1 = view.findViewById<ImageButton>(R.id.rus)
-        val bF2 = view.findViewById<ImageButton>(R.id.kaz)
-        bF1.setOnClickListener {  }
-        bF2.setOnClickListener {  }
+        val mF = view.findViewById<ImageButton>(R.id.startButton)
+        mF.setOnClickListener {  }
     }
 }
