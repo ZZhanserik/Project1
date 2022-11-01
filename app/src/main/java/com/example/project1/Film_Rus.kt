3c1e2ccb4d5d99b47eeade44f1cd43d8a9782fb3
ZZhanserik <200107046@stu.sdu.edu.kz>
2022-11-02 02:02:21 +0600

    commit nav1

--- app/src/main/java/com/example/project1/Film_Rus.kt
+++ app/src/main/java/com/example/project1/Film_Rus.kt
@@ -6,13 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class Catalog : Fragment() {
+
+class Film_Rus : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.catalog, container, false)
+        return inflater.inflate(R.layout.films_rus, container, false)
     }
 
 }
