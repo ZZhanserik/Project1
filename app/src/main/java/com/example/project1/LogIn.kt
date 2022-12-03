e10f6611aa7d41046032e02c41ced5965b98e05c
Nuraiym Zhumagul <nuraiii2002222@gmail.com>
2022-12-03 23:34:38 +0600

    ROOM 4

---
+++ app/src/main/java/com/example/project1/LogIn.kt
@@ -0,0 +1,14 @@
+package com.example.project1
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_inf_table")
+
+data class LogIn(
+    @PrimaryKey(autoGenerate = true) val id: Int?,
+    @ColumnInfo(name = "first_name") val firstName: String?,
+    @ColumnInfo(name = "last_name") val lastName: String?,
+    @ColumnInfo(name = "password") val password: Int?
+)
