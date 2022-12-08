c484b841b1534a619b16eb8575850bb6741abf9c
Nuraiym Zhumagul <nuraiii2002222@gmail.com>
2022-12-09 05:19:19 +0600

    add DI with koin, dependencies, viewModel - Noorah

--- app/src/main/java/com/example/project1/presentation/Catalog.kt
+++ app/src/main/java/com/example/project1/presentation/Catalog.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
-import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.project1.R
 class Catalog : Fragment() {
--- app/src/main/java/com/example/project1/presentation/MainFragment.kt
+++ app/src/main/java/com/example/project1/presentation/MainFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import com.example.project1.R
 
 class MainFragment : Fragment() {
